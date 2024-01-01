a47f6ec4272299a712159614dca47218dd3ca8da
Kraktoos <kraktoos@gmail.com>
2024-01-01 20:27:30 +0000

    Update app icons and dependencies

--- context/SupabaseProvider.tsx
+++ context/SupabaseProvider.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 
 import { EmailOtpType, Session, createClient } from "@supabase/supabase-js";
 import * as SecureStore from "expo-secure-store";
+import * as aesjs from "aes-js";
+import "react-native-get-random-values";
 import { useSegments, useRouter, useRootNavigationState } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Constants from "expo-constants";
@@ -10,18 +12,59 @@ import * as SplashScreen from "expo-splash-screen";
 
 import { SupabaseContext } from "./SupabaseContext";
 
-// We are using Expo Secure Store to persist session info
-const ExpoSecureStoreAdapter = {
-  getItem: (key: string) => {
-    return SecureStore.getItemAsync(key);
-  },
-  setItem: (key: string, value: string) => {
-    SecureStore.setItemAsync(key, value);
-  },
-  removeItem: (key: string) => {
-    SecureStore.deleteItemAsync(key);
-  },
-};
+class LargeSecureStore {
+  private async _encrypt(key: string, value: string) {
+    const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8));
+
+    const cipher = new aesjs.ModeOfOperation.ctr(
+      encryptionKey,
+      new aesjs.Counter(1)
+    );
+    const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value));
+
+    await SecureStore.setItemAsync(
+      key,
+      aesjs.utils.hex.fromBytes(encryptionKey)
+    );
+
+    return aesjs.utils.hex.fromBytes(encryptedBytes);
+  }
+
+  private async _decrypt(key: string, value: string) {
+    const encryptionKeyHex = await SecureStore.getItemAsync(key);
+    if (!encryptionKeyHex) {
+      return encryptionKeyHex;
+    }
+
+    const cipher = new aesjs.ModeOfOperation.ctr(
+      aesjs.utils.hex.toBytes(encryptionKeyHex),
+      new aesjs.Counter(1)
+    );
+    const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value));
+
+    return aesjs.utils.utf8.fromBytes(decryptedBytes);
+  }
+
+  async getItem(key: string) {
+    const encrypted = await AsyncStorage.getItem(key);
+    if (!encrypted) {
+      return encrypted;
+    }
+
+    return await this._decrypt(key, encrypted);
+  }
+
+  async removeItem(key: string) {
+    await AsyncStorage.removeItem(key);
+    await SecureStore.deleteItemAsync(key);
+  }
+
+  async setItem(key: string, value: string) {
+    const encrypted = await this._encrypt(key, value);
+
+    await AsyncStorage.setItem(key, encrypted);
+  }
+}
 
 // This hook will protect the route access based on user authentication.
 function useProtectedRoute(isLoggedIn: boolean) {
@@ -62,7 +105,7 @@ export const SupabaseProvider = (props: SupabaseProviderProps) => {
     Constants.expoConfig?.extra?.EXPO_PUBLIC_SUPABASE_ANON_KEY as string,
     {
       auth: {
-        storage: ExpoSecureStoreAdapter,
+        storage: new LargeSecureStore(),
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
@@ -136,14 +179,18 @@ export const SupabaseProvider = (props: SupabaseProviderProps) => {
   const [session, setSession] = React.useState<Session | null>(null);
 
   React.useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
+    supabase.auth.getSession().then(({ data: { session: _session } }) => {
+      setSession(_session);
     });
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
+    supabase.auth.onAuthStateChange((_event, _session) => {
+      setSession(_session);
     });
   }, []);
+
+  const useSession = () => {
+    return session;
+  };
 
   // React.useEffect(() => {
   //   (async () => {
