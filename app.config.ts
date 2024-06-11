1c4942a5fcdc9f90f1da4276c1533293729afc04
Kraktoos <kraktoos@gmail.com>
2024-06-11 13:01:22 +0100

    Update to fix bugs?

--- app.config.ts
+++ app.config.ts
@@ -54,6 +54,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       favicon: "./assets/images/favicon.png",
     },
     plugins: [
+      "expo-secure-store",
       [
         "expo-dynamic-app-icon",
         {
